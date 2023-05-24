d3d5f4686c3f502fdea75bbeb0eb4107b1cc49fe
Marcos santos <vynni.mv@hotmail.com>
2023-05-24 18:27:02 -0300

    Commit initial

---
+++ Listas_Java_POO/src/Lista1/GravidadeNaLua.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package Lista1;
+
+import java.util.Scanner;
+
+/**
+ * @author Marcos Vinicius
+ *
+ */
+public class GravidadeNaLua {
+	public static void main(String[] args) {
+		/*3 --> A gravidade da Lua é cerca de 17% do da Terra.
+		 * Faça um programa em Java que calcule seu peso na Lua.
+		 */
+		Scanner input = new Scanner(System.in);
+		double pesoNaLua;
+		double gravidadeDaLua = 1.63;
+		
+		//
+		System.out.println("Digite seu peso: ");
+		double peso = input.nextDouble();
+		
+		pesoNaLua = gravidadeDaLua * (peso/9.81);
+		
+		System.out.printf("Seu peso na superficie da lua é de: %.2fKg" ,pesoNaLua);
+	
+		input.close();
+	}
+
+}
