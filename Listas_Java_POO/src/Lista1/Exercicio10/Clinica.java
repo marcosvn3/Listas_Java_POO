fa4f4061558a53dfad49ec7b5c4138a74b17ec06
Marcos santos <vynni.mv@hotmail.com>
2023-05-24 19:57:07 -0300

    Concluido

---
+++ Listas_Java_POO/src/Lista1/Exercicio10/Clinica.java
@@ -0,0 +1,16 @@
+package Lista1.Exercicio10;
+
+public class Clinica {
+	public static void main(String[] args) {
+		Paciente paciente1 = new Paciente("Marocs","M",24,73,1.84);
+		
+		;
+		
+		System.out.printf("Valor da consulta e de: %.2fR$.",valorConsulta(paciente1));
+	}
+	
+	public static double valorConsulta(Paciente p){
+		double valor = p.getAltura()*(p.getAltura()+p.getIdade());
+		return valor;
+	}
+}
---
+++ Listas_Java_POO/src/Lista1/Exercicio10/Paciente.java
@@ -0,0 +1,56 @@
+package Lista1.Exercicio10;
+
+public class Paciente {
+	private String nome;
+	private String sexo;
+	private double peso;
+	private int idade;
+	private double altura;
+	
+	public Paciente() {}
+	
+	public Paciente(String nome, String sexo, int idade, double peso, double altura) {
+		this.nome= nome;
+		this.sexo = sexo;
+		this.idade = idade;
+		this.peso = peso;
+		this.altura= altura;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	public void setNome(String nome) {
+		this.nome=nome;
+	}
+	
+	public String getSexo() {
+		return this.sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+	public int getIdade() {
+		return this.idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	public double getPeso() {
+		return this.peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
+	public double getAltura() {
+		return this.altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	
+}
