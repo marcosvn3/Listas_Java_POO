6c9a985eb73faa2083ef42f9637358f6a26fcbad
Marcos santos <vynni.mv@hotmail.com>
2023-05-24 18:44:14 -0300

    conclusao do prog

---
+++ Listas_Java_POO/src/Lista1/AreaCirculo.java
@@ -0,0 +1,29 @@
+package Lista1;
+
+import java.util.Scanner;
+
+public class AreaCirculo {
+	/**
+	 * @author Marcos vinicius
+	 *
+	 */
+	public static void main(String[] args) {
+		/*
+		 * 5 -->Escreva um programa em Java que leia o valor do raio
+		 * e imprima a área do círculo.
+		 */
+		
+		Scanner input = new Scanner(System.in);
+		double areaCirculo, raio;
+		final double PI = 3.1415;
+		
+		System.out.println("Digite o raio do circulo: ");
+		raio = input.nextDouble();
+		
+		areaCirculo = (raio*raio)*PI;
+		
+		System.out.printf("Area do circulo e de: %.2f", areaCirculo);
+		
+		input.close();
+	}
+}
