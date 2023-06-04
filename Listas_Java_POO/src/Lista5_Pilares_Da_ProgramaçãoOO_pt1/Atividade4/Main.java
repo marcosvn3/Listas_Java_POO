8d38cbe8f3aec5eabb4022ba3558e209cd273c6c
Marcos santos <vynni.mv@hotmail.com>
2023-06-04 15:26:18 -0300

    Concluido

--- Listas_Java_POO/src/Lista5_Pilares_Da_ProgramaçãoOO_pt1/Atividade4/Main.java
+++ Listas_Java_POO/src/Lista5_Pilares_Da_ProgramaçãoOO_pt1/Atividade4/Main.java
@@ -1,21 +1,30 @@
 package Lista5_Pilares_Da_ProgramaçãoOO_pt1.Atividade4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 	public static void main(String[] args) {
+		List<PessoaIMC> clientes = new ArrayList<>();
+		
 		Homem h1 = new Homem("Marcos", "15/03/1999", 73, 1.84);
-		System.out.println(h1);
-		h1.calcularIMC();
-		h1.resultIMC();
-		
-		
-		
-		System.out.println("_--------------------------------------------_");
-		
 		Mulher m1 = new Mulher("Laura Fiel", "01/10/2001", 47, 1.66);
-		System.out.println(m1);
-		m1.calcularIMC();
-		m1.resultIMC();
+		Homem h2 = new Homem("Fiel","15/09/1965",97.5,1.85);
+		Mulher m2 = new Mulher("Maria jose", "18/11/1965", 74, 1.59);
+		Mulher m3 = new Mulher("Layla Fiel", "22/10/1998", 58, 1.62);
+		
+		clientes.add(h1);
+		clientes.add(h2);
+		clientes.add(m1);
+		clientes.add(m2);
+		clientes.add(m3);
 		
 		
+		for(PessoaIMC pessoa : clientes) {
+			System.out.println(pessoa);
+			pessoa.resultIMC();
+			System.out.println("---------------------");
+			
+		}
 	}
 }
